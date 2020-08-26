b4b6d4289fd0886bb9d70d58e4a4b9c3e9e39303
CHarel <Charel@bk.ru>
2020-08-26 11:26:09 +0300

    Crate ViewModelDelegate

---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
@@ -0,0 +1,18 @@
+package ru.skillbranch.skillarticles.viewmodels.base
+
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class ViewModelDelegate<T: ViewModel>(private val clazz: Class<T>, private val arg: Any?):ReadOnlyProperty<FragmentActivity, T> {
+    override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
+        return if (arg != null) {
+            val vmFactory = ViewModelFactory(arg)
+            ViewModelProviders.of(thisRef, vmFactory).get(clazz)
+        }else{
+            ViewModelProviders.of(thisRef).get(clazz)
+        }
+    }
+}
