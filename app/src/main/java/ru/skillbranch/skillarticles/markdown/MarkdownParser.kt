40c9acb901c81fb68add3c9380ff38c23b9aa809
CHarel <Charel@bk.ru>
2020-09-04 14:35:34 +0300

    Getting started whith a lesson

---
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -0,0 +1,221 @@
+package ru.skillbranch.skillarticles.markdown
+
+import java.util.regex.Pattern
+
+object MarkdownParser {
+
+    private val LINE_SEPARATOR = "\n"
+
+    //group regex
+    private const val UNORDERED_LIST_ITEM_GROUP = "(^[*+-] .+$)"
+    private const val HEADER_GROUP = "(^#{1,6} .+?$)"
+    private const val QUOTE_GROUP = "(^> .+?$)"
+    private const val ITALIC_GROUP = "((?<!\\*)\\*[^*].*?[^*]?\\*(?!\\*)|(?<!_)_[^_].*?[^_]?_(?!_))"
+    private const val BOLD_GROUP ="" //TODO implement me
+    private const val STRIKE_GROUP = "" //TODO implement me
+    private const val RULE_GROUP = "" //TODO implement me
+    private const val INLINE_GROUP = "" //TODO implement me
+    private const val LINK_GROUP = "" //TODO implement me
+    private const val BLOCK_CODE_GROUP = "" //TODO implement me
+    private const val ORDER_LIST_GROUP = "" //TODO implement me
+
+    //result regex
+    private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP" +
+            "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP"
+    //|$BLOCK_CODE_GROUP|$ORDER_LIST_GROUP optionally
+
+    private val elementsPattern by lazy { Pattern.compile(MARKDOWN_GROUPS, Pattern.MULTILINE) }
+
+    /**
+     * parse markdown text to elements
+     */
+    fun parse(string: String): MarkdownText {
+        val elements = mutableListOf<Element>()
+        elements.addAll(findElements(string))
+        return MarkdownText(elements)
+    }
+
+    /**
+     * clear markdown text to string without markdown characters
+     */
+    fun clear(string: String?): String? {
+        //TODO implement me
+
+    }
+
+    /**
+     * find markdown elements in markdown text
+     */
+    private fun findElements(string: CharSequence): List<Element> {
+        val parents = mutableListOf<Element>()
+        val matcher = elementsPattern.matcher(string)
+        var lastStartIndex = 0
+
+        loop@ while (matcher.find(lastStartIndex)) {
+            val startIndex = matcher.start()
+            val endIndex = matcher.end()
+
+            //if something is found then everything before -TEXT
+            if (lastStartIndex < startIndex){
+                parents.add(Element.Text(string.subSequence(lastStartIndex,startIndex)))
+            }
+
+            //found text
+            var text: CharSequence
+
+            //groups range for iterate by groups (1..9) or (1..11) optionally
+            val groups = 1..9
+            var group = -1
+            for (gr in groups){
+                if (matcher.group(gr)!= null){
+                    group =gr
+                    break
+                }
+            }
+
+            when (group) {
+                //NOT FOUND -> BREAK
+                -1 -> break@loop
+
+                //UNORDERED LIST
+                1 -> {
+                    //text without "*. "
+                    //TODO implement me
+                }
+
+                //HEADER
+                2 -> {
+                    //text without "{#} "
+                    //TODO implement me
+                }
+
+                //QUOTE
+                3 -> {
+                    //text without "> "
+                    //TODO implement me
+                }
+
+                //ITALIC
+                4 -> {
+                    //text without "*{}*"
+                    //TODO implement me
+                }
+
+                //BOLD
+                5 -> {
+                    //text without "**{}**"
+                    //TODO implement me
+                }
+
+                //STRIKE
+                6 -> {
+                    //text without "~~{}~~"
+                    //TODO implement me
+                }
+
+                //RULE
+                7 -> {
+                    //text without "***" insert empty character
+                    //TODO implement me
+                }
+
+                //RULE
+                8 -> {
+                    //text without "`{}`"
+                    //TODO implement me
+                }
+
+                //LINK
+                9 -> {
+                    //full text for regex
+                    //TODO implement me
+                }
+                //10 -> BLOCK CODE - optionally
+                10 -> {
+                    //TODO implement me
+                }
+
+                //11 -> NUMERIC LIST
+                11 -> {
+                    //TODO implement me
+                }
+            }
+
+        }
+
+        //TODO implement me
+    }
+}
+
+data class MarkdownText(val elements: List<Element>)
+
+sealed class Element() {
+    abstract val text: CharSequence
+    abstract val elements: List<Element>
+
+    data class Text(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class UnorderedListItem(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Header(
+        val level: Int = 1,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Quote(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Italic(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Bold(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Strike(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Rule(
+        override val text: CharSequence = " ", //for insert span
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class InlineCode(
+        override val text: CharSequence, //for insert span
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class Link(
+        val link: String,
+        override val text: CharSequence, //for insert span
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class OrderedListItem(
+        val order: String,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element()
+
+    data class BlockCode(
+        val type: Type = Type.MIDDLE,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ) : Element() {
+        enum class Type { START, END, MIDDLE, SINGLE }
+    }
+}
